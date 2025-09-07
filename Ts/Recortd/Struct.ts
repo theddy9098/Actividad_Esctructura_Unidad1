c57a999ae95a55969f075ca6114bb17c3a609f97
theddy9098 <teddyfso15@gmail.com>
2025-09-07 18:39:07 -0500

    Definicion de un Record

---
+++ Ts/Recortd/Struct.ts
@@ -0,0 +1,14 @@
+type estudiante = {
+  nombre: string;
+  edad: number;
+  promedio: number;
+};
+
+
+const persona1: estudiante = {
+  nombre: "Juan",
+  edad: 30,
+  promedio: 4.5,
+};
+
+console.log(persona1);
