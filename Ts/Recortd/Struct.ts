560d8f2fa194e501398c9922bcb9032f49157571
theddy9098 <teddyfso15@gmail.com>
2025-09-07 19:59:56 -0500

    Agregar clase Persona y crear instancias

--- Ts/Recortd/Struct.ts
+++ Ts/Recortd/Struct.ts
@@ -12,3 +12,21 @@ const persona1: estudiante = {
 };
 
 console.log(persona1);
+
+class Persona {
+  nombre: string    
+    edad: number
+    promedio: number
+
+    constructor(nombre: string, edad: number, promedio: number) {
+        this.nombre = nombre
+        this.edad = edad
+        this.promedio = promedio
+    }
+}
+
+let persona2 = new Persona("Pedro", 25, 4.7)
+const persona3 = new Persona("Maria", 28, 4.9)
+
+console.log(persona2)
+console.log(persona3)
