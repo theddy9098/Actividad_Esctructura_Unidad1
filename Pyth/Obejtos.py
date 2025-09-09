eac54b7aace9e34f9a0c9ca5b1e7cc80770885d2
theddy9098 <teddyfso15@gmail.com>
2025-09-08 19:01:57 -0500

    Instancias y arreglo

--- Pyth/Obejtos.py
+++ Pyth/Obejtos.py
@@ -11,6 +11,12 @@ class Estudiantes:
 
 
 estudiante1= Estudiantes ("Ana", 20, 8.5)
-estudiante1.mostrarInfo()
+estudiante2= Estudiantes ("Luis", 22, 9.0)
+estudiante3= Estudiantes ("Marta", 19, 7.5)
+estudiante4= Estudiantes ("Jorge", 21, 8.0)
 
+Estudiantes_lista = [estudiante1, estudiante2, estudiante3, estudiante4]
+
+for i in Estudiantes_lista:
+    i.mostrarInfo() 
         
